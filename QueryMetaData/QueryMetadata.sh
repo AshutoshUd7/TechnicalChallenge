9ffdec03ae249f54f959049bad5e18887af2c496
Ashutosh Udayasingh <75110837+AshutoshUd7@users.noreply.github.com>
2020-12-05 21:13:55 +0530

    Update QueryMetadata.sh

--- QueryMetaData/QueryMetadata.sh
+++ QueryMetaData/QueryMetadata.sh
@@ -1,3 +1,8 @@
-sudo apt gt install curl
-sudo apt get install jq
+#Install Curl Dependencies
+sudo apt-get install curl
+
+#Install JSON Dependencies
+sudo apt-get install jq
+
+#Querying Azure Instance metadata
 curl -H Metadata:True "http://169.254.169.254/metadata/instance?api-version=2020-09-01&formst=json" | jq .
